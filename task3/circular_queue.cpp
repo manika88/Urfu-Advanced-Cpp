c154de8844f1a32a01f14226498bb56d0ca5a85f
Григорий Севергин <g.severgin@vk.team>
2024-11-13 11:00:45 +0500

    add task3

---
+++ task3/circular_queue.cpp
@@ -0,0 +1,36 @@
+#include "circular_queue.hpp"
+
+CircularQueue::CircularQueue(size_t size)
+{
+	// your implementation here
+}
+
+bool CircularQueue::Push(int value)
+{
+    // your implementation here
+}
+
+bool CircularQueue::Pop()
+{
+    // your implementation here
+}
+
+int CircularQueue::Front() const
+{
+    // your implementation here
+}
+
+int CircularQueue::Back() const
+{
+    // your implementation here
+}
+
+bool CircularQueue::Empty() const
+{
+    // your implementation here
+}
+
+bool CircularQueue::Full() const
+{
+    // your implementation here
+}
