94f56d227b4761af5f920f12d31a9e17d0aba010
manika88 <gasilovamarij184@gmail.com>
2025-01-01 00:00:42 +0500

    Update matrix.cpp

--- task1/matrix.cpp
+++ task1/matrix.cpp
@@ -4,45 +4,57 @@
 
 Matrix::Matrix(int numRows, int numCols)
 {
-    // your implementation here
+    Reset(numRows, numCols);
 }
 
 void Matrix::Reset(int numRows, int numCols)
 {
-    // your implementation here
+    if (numRows == 0 || numCols == 0) 
+    {
+        row = col = 0;
+    }
 }
 
 int& Matrix::At(int row, int col)
 {
-    // your implementation here
+    if (row < 0 || row >=numRows  || col < 0 || col >= numCols) 
+    {
+        throw std::out_of_range;
+    }
 }
 
 const int& Matrix::At(int row, int col) const
 {
-    // your implementation here
+    if (row < 0 || row >=numRows  || col < 0 || col >= numCols) 
+    {
+        throw std::out_of_range;
+    }
 }
 
 int Matrix::GetRows() const
 {
-    // your implementation here
+    return numRows;
 }
 
 int Matrix::GetCols() const
 {
-    // your implementation here
+    return numCols;
 }
 
 bool Matrix::operator==(const Matrix& m2)
 {
-    // your implementation here
+    return  == m2.numRows && cols == m2.numCols && data == m2.data;
 }
 
 bool Matrix::operator!=(const Matrix& m2)
 {
-    // your implementation here
+    return !(*this == m2);
 }
 
 Matrix Matrix::operator+(const Matrix& m2)
 {
-    // your implementation here
+    if (row != m2.numRows || col != m2.numCols)
+    {
+        throw std::invalid_argument;
+    }
 }
